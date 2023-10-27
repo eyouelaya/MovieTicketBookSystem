9ffb5704767d6614c3ee281e5e1fd18b37cde1cd
Eyouel <eyouelaya@gmail.com>
2023-10-27 17:26:07 -0500

    fix pop up for movie selection

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -39,13 +39,14 @@ document
     var container = document.querySelector(".scrollable-movie-cards-latest");
     container.scrollLeft += 500; // Assuming each card is 200px wide
   });
-
+var movieCollection;
 async function getMovies() {
   try {
     let response = await fetch("http://localhost:3000/movies");
     if (response.ok) {
       let movies = await response.json();
       console.log(movies);
+      movieCollection = movies;
       loadMainCard(movies);
       loadMoviesToday(movies);
     }
@@ -76,6 +77,10 @@ function loadMainCard(movies) {
     recent[2]["trailerLink"];
   document.getElementById("mainCardTrailerLink4").href =
     recent[3]["trailerLink"];
+  document.getElementById("mainCardBook1").value = recent[0]["_id"];
+  document.getElementById("mainCardBook2").value = recent[1]["_id"];
+  document.getElementById("mainCardBook3").value = recent[2]["_id"];
+  document.getElementById("mainCardBook4").value = recent[3]["_id"];
 }
 
 function loadMoviesToday(movies) {
@@ -108,6 +113,22 @@ function loadMoviesToday(movies) {
     today[1]["runningTime"];
   document.getElementById("todayMovieImage5").style.backgroundImage =
     "url('http://localhost:3000/" + today[4]["imageLink"] + "')";
+
+  document
+    .getElementById("todayCardBook1")
+    .setAttribute("value", today[0]["_id"]);
+  document
+    .getElementById("todayCardBook2")
+    .setAttribute("value", today[1]["_id"]);
+  document
+    .getElementById("todayCardBook3")
+    .setAttribute("value", today[2]["_id"]);
+  document
+    .getElementById("todayCardBook4")
+    .setAttribute("value", today[3]["_id"]);
+  document
+    .getElementById("todayCardBook5")
+    .setAttribute("value", today[4]["_id"]);
 }
 
 function loadLastestRelease(movies) {
@@ -125,3 +146,105 @@ document
     // Now you can use inputValue, which will contain the text as it's being entered
     console.log("Current input:", inputValue);
   });
+
+const modal = document.getElementById("movieModal");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+document.getElementById("mainCardBook1").addEventListener("click", (event) => {
+  modal.style.display = "block";
+  movie = movieCollection.filter((m) => m._id == event.target.value);
+  setValueToModal(movie);
+});
+document.getElementById("mainCardBook2").addEventListener("click", (event) => {
+  modal.style.display = "block";
+  movie = movieCollection.filter((m) => m._id == event.target.value);
+  setValueToModal(movie);
+});
+document.getElementById("mainCardBook3").addEventListener("click", (event) => {
+  modal.style.display = "block";
+  movie = movieCollection.filter((m) => m._id == event.target.value);
+  setValueToModal(movie);
+});
+document.getElementById("mainCardBook4").addEventListener("click", (event) => {
+  modal.style.display = "block";
+  movie = movieCollection.filter((m) => m._id == event.target.value);
+  setValueToModal(movie);
+});
+
+document.getElementById("todayCardBook1").addEventListener("click", (event) => {
+  // event.preventDefault();
+  modal.style.display = "block";
+  movie = movieCollection.filter(
+    (m) => m._id == event.currentTarget.getAttribute("value")
+  );
+  console.log();
+  setValueToModal(movie);
+});
+
+document.getElementById("todayCardBook2").addEventListener("click", (event) => {
+  // event.preventDefault();
+  modal.style.display = "block";
+  movie = movieCollection.filter(
+    (m) => m._id == event.currentTarget.getAttribute("value")
+  );
+  console.log();
+  setValueToModal(movie);
+});
+document.getElementById("todayCardBook3").addEventListener("click", (event) => {
+  // event.preventDefault();
+  modal.style.display = "block";
+  movie = movieCollection.filter(
+    (m) => m._id == event.currentTarget.getAttribute("value")
+  );
+  console.log();
+  setValueToModal(movie);
+});
+document.getElementById("todayCardBook4").addEventListener("click", (event) => {
+  // event.preventDefault();
+  modal.style.display = "block";
+  movie = movieCollection.filter(
+    (m) => m._id == event.currentTarget.getAttribute("value")
+  );
+  console.log();
+  setValueToModal(movie);
+});
+document.getElementById("todayCardBook5").addEventListener("click", (event) => {
+  // event.preventDefault();
+  modal.style.display = "block";
+  movie = movieCollection.filter(
+    (m) => m._id == event.currentTarget.getAttribute("value")
+  );
+  console.log();
+  setValueToModal(movie);
+});
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+function setValueToModal(movie) {
+  console.log(movie);
+  console.log("http://localhost:3000/" + movie[0].imageLink);
+  document.getElementById("modalMovieImage").src =
+    "http://localhost:3000/" + movie[0].imageLink;
+  console.log(movie[0].title);
+  document.getElementById("modalMovieName").textContent = movie[0].title;
+  document.getElementById("modalReleaseDate").textContent = parseDate(
+    movie[0].releaseDate
+  );
+  document.getElementById("modalRuntime").textContent = movie[0].runningTime;
+  document.getElementById("modalGenre").textContent = movie[0].genre;
+  document.getElementById("modalAboutMovie").textContent = movie[0].description;
+  document.getElementById("modalMovieTrailer").href = movie[0].trailerLink;
+  document.getElementById("modalbookMovie").value = movie[0]._id;
+}
+
+document.getElementById("modalbookMovie").addEventListener("click", (event) => {
+  window.open("pages/booking.html?id=" + event.target.value, "_blank");
+});
+
+function parseDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1; // Months are zero-based
+  return `${year}-${month.toString().padStart(2, "0")}`;
+}
