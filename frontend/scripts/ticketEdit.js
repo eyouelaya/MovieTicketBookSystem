23fddc86fca014bd48c01830c2f372a44105e1f1
yohannnes <johnysg064@gmail.com>
2023-10-27 23:13:00 -0500

    redirecting after editing ticket to ticket management

--- frontend/scripts/ticketEdit.js
+++ frontend/scripts/ticketEdit.js
@@ -1,140 +1,138 @@
-const timeSlotsContainer = document.getElementById('time-slots')
+const timeSlotsContainer = document.getElementById("time-slots");
 
-const startTime = 8
-const endTime = 18
-
-const interval = 1
+const startTime = 8;
+const endTime = 18;
+const interval = 1;
 
 for (let i = startTime; i < endTime; i += interval) {
-  const timeSlot = document.createElement('div')
-  timeSlot.className = 'col-md-2 p-3'
+  const timeSlot = document.createElement("div");
+  timeSlot.className = "col-md-2 p-3";
   const time =
-    i % 12 === 0 ? '12:00 PM' : `${i % 12}:00 ${i < 12 ? 'AM' : 'PM'}`
+    i % 12 === 0 ? "12:00 PM" : `${i % 12}:00 ${i < 12 ? "AM" : "PM"}`;
 
-  const timeBox = document.createElement('div')
-  timeBox.className = 'time-box'
-  timeBox.textContent = time
+  const timeBox = document.createElement("div");
+  timeBox.className = "time-box";
+  timeBox.textContent = time;
 
-  timeSlot.appendChild(timeBox)
+  timeSlot.appendChild(timeBox);
 
-  timeSlotsContainer.appendChild(timeSlot)
+  timeSlotsContainer.appendChild(timeSlot);
 }
 
-const dateSlotsContainer = document.getElementById('date-slots')
+const dateSlotsContainer = document.getElementById("date-slots");
 
 function getNext8Days() {
-  const days = []
-  const today = new Date()
+  const days = [];
+  const today = new Date();
 
   for (let i = 0; i < 8; i++) {
-    const date = new Date(today)
-    date.setDate(today.getDate() + i)
-    days.push(date)
+    const date = new Date(today);
+    date.setDate(today.getDate() + i);
+    days.push(date);
   }
 
-  return days
+  return days;
 }
 
-const daysOfWeek = getNext8Days()
+const daysOfWeek = getNext8Days();
 
 for (let i = 0; i < 7; i++) {
-  const dateSlot = document.createElement('div')
-  dateSlot.className = 'col-md-3 p-3'
+  const dateSlot = document.createElement("div");
+  dateSlot.className = "col-md-3 p-3";
 
-  const carouselCell = document.createElement('div')
-  carouselCell.className = 'carousel-cell'
+  const carouselCell = document.createElement("div");
+  carouselCell.className = "carousel-cell";
 
-  const date = document.createElement('div')
-  date.className = 'date-numeric'
-  date.textContent = daysOfWeek[i + 1].getDate()
+  const date = document.createElement("div");
+  date.className = "date-numeric";
+  date.textContent = daysOfWeek[i + 1].getDate();
 
-  const dayOfWeek = document.createElement('div')
-  dayOfWeek.className = 'date-day'
-  dayOfWeek.textContent = daysOfWeek[i + 1].toLocaleString('en-US', {
-    weekday: 'long',
-  })
+  const dayOfWeek = document.createElement("div");
+  dayOfWeek.className = "date-day";
+  dayOfWeek.textContent = daysOfWeek[i + 1].toLocaleString("en-US", {
+    weekday: "long",
+  });
 
-  carouselCell.appendChild(date)
-  carouselCell.appendChild(dayOfWeek)
-  dateSlot.appendChild(carouselCell)
+  carouselCell.appendChild(date);
+  carouselCell.appendChild(dayOfWeek);
+  dateSlot.appendChild(carouselCell);
 
-  dateSlotsContainer.appendChild(dateSlot)
+  dateSlotsContainer.appendChild(dateSlot);
 }
 
-let selectedDate = null
-let selectedTime = null
+let selectedDate = null;
+let selectedTime = null;
 // Get the current URL
 const currentURL = window.location.href;
-const parts = currentURL.split('=');
+const parts = currentURL.split("=");
 const id = parts[parts.length - 1];
 
-const timeBoxes = document.querySelectorAll('.time-box')
+const timeBoxes = document.querySelectorAll(".time-box");
 
 timeBoxes.forEach((box) => {
-  box.addEventListener('click', () => {
-    timeBoxes.forEach((item) => item.classList.remove('selected'))
+  box.addEventListener("click", () => {
+    timeBoxes.forEach((item) => item.classList.remove("selected"));
 
-    box.classList.add('selected')
-    selectedTime = box.textContent
-    console.log(selectedTime)
-    updateButtonState()
-  })
-})
+    box.classList.add("selected");
+    selectedTime = box.textContent;
+    console.log(selectedTime);
+    updateButtonState();
+  });
+});
 
-const dateCells = document.querySelectorAll('.carousel-cell')
+const dateCells = document.querySelectorAll(".carousel-cell");
 
 dateCells.forEach((cell) => {
-  cell.addEventListener('click', () => {
-    dateCells.forEach((item) => item.classList.remove('selected'))
+  cell.addEventListener("click", () => {
+    dateCells.forEach((item) => item.classList.remove("selected"));
 
-    cell.classList.add('selected')
-    selectedDate = cell.querySelector('.date-numeric').textContent
-    selectedDateDay = cell.querySelector('.date-day').textContent
+    cell.classList.add("selected");
+    selectedDate = cell.querySelector(".date-numeric").textContent;
+    selectedDateDay = cell.querySelector(".date-day").textContent;
     // console.log(selectedDate)
     // console.log(selectedDateDay)
-    updateButtonState()
-  })
-})
+    updateButtonState();
+  });
+});
 
 function updateButtonState() {
   if (selectedDate && selectedTime) {
-    continueButton.classList.remove('disabled')
-    continueButton.disabled = false
+    continueButton.classList.remove("disabled");
+    continueButton.disabled = false;
   } else {
-    continueButton.classList.add('disabled')
-    continueButton.disabled = true
+    continueButton.classList.add("disabled");
+    continueButton.disabled = true;
   }
 }
 
-const continueButton = document.getElementById('continueButton')
+const continueButton = document.getElementById("continueButton");
 
-continueButton.addEventListener('click', async function () {
-
+continueButton.addEventListener("click", async function () {
   // Get the current URL
-const currentURL = window.location.href;
-const parts = currentURL.split('=');
-const id = parts[parts.length - 1];
+  const currentURL = window.location.href;
+  const parts = currentURL.split("=");
+  const id = parts[parts.length - 1];
 
-    const response = await fetch(`http://localhost:3000/tickets/${id}`)
-    if(response.ok){
+  const response = await fetch(`http://localhost:3000/tickets/${id}`);
+  if (response.ok) {
     let ticket = await response.json();
-  let date = selectedDate + ' ' + selectedDateDay + ' ' + selectedTime
-  const ticketData = {
-    movieDate: date,
-  }
-  const response2 = await fetch(`http://localhost:3000/tickets/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(ticketData),
-  })
-if(response2){
-    const data = await response2.json()
-    alert("ticket edited sucessfully!")
-  } else {
-    alert('not edited try again')
-}
-
+    let date = selectedDate + " " + selectedDateDay + " " + selectedTime;
+    const ticketData = {
+      movieDate: date,
+    };
+    const response2 = await fetch(`http://localhost:3000/tickets/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ticketData),
+    });
+    if (response2) {
+      const ticket = await response2.json();
+      alert("ticket edited sucessfully!");
+      window.open("manageTicket.html?id=" + ticket.code);
+    } else {
+      alert("not edited try again");
     }
-})
+  }
+});
--- frontend/scripts/ticketManagement.js
+++ frontend/scripts/ticketManagement.js
@@ -1,14 +1,26 @@
 // Function to populate the table with tickets
 // Store a Set to keep track of displayed ticket codes
-const displayedTicketCodes = new Set();
 
+const displayedTicketCodes = new Set();
+window.onload = async function () {
+  const currentURL = window.location.href;
+  const parts = currentURL.split("=");
+  const id = parts[1];
+  if (id) {
+    const response = await fetch(`http://localhost:3000/tickets/${id}`);
+    if (response.ok) {
+      let ticket = await response.json();
+      populateTable(ticket);
+    }
+  }
+};
 // Function to populate the table with tickets, checking if it has been displayed
 const populateTable = (ticket) => {
-    const ticketTable = document.getElementById('ticketTable');
-    
-    if (!displayedTicketCodes.has(ticket.code)) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+  const ticketTable = document.getElementById("ticketTable");
+
+  if (!displayedTicketCodes.has(ticket.code)) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${ticket.code}</td>
             <td>${ticket.movieName}</td>
             <td>${ticket.runningTime}</td>
@@ -18,65 +30,61 @@ const populateTable = (ticket) => {
                 <button class="delete-button" onclick="deleteTicket('${ticket.code}')">Delete</button>
             </td>
         `;
-        ticketTable.appendChild(row);
-        displayedTicketCodes.add(ticket.code);
-    }
+    ticketTable.appendChild(row);
+    displayedTicketCodes.add(ticket.code);
+  }
 };
 
 // Function to fetch and display all tickets
 async function getTickets() {
-    const response = await fetch('http://localhost:3000/tickets')
-        if(response.ok){
-            let ticket = await response.json();
-            populateTable(ticket);
-        }
-};
+  const response = await fetch("http://localhost:3000/tickets");
+  if (response.ok) {
+    let ticket = await response.json();
+    populateTable(ticket);
+  }
+}
 
 // Function to search for tickets
 async function searchTicket() {
-    const searchInput = document.getElementById('searchInput').value;
-    const response= await fetch(`http://localhost:3000/tickets/${searchInput}`)
-        if (!response.ok) {
-            // Handle the case where the ticket is not found
-            alert('ticket number: '+searchInput+' not found');
-        } else {
-            let ticket = await response.json();
-            if(!ticket.movieName)
-            return;
-            populateTable(ticket);
-        }
+  const searchInput = document.getElementById("searchInput").value;
+  const response = await fetch(`http://localhost:3000/tickets/${searchInput}`);
+  if (!response.ok) {
+    // Handle the case where the ticket is not found
+    alert("ticket number: " + searchInput + " not found");
+  } else {
+    let ticket = await response.json();
+    if (!ticket.movieName) return;
+    populateTable(ticket);
+  }
 }
-
-
 
 // Function to delete a ticket
 async function deleteTicket(ticketId) {
-    const response = await fetch(`http://localhost:3000/tickets/${ticketId}`, {
-        method: 'DELETE',
-    });
+  const response = await fetch(`http://localhost:3000/tickets/${ticketId}`, {
+    method: "DELETE",
+  });
 
-    if (response.ok) {
-        alert("Ticket deleted successfully!");
-        // Find and remove the table row for the deleted ticket
-        const ticketTable = document.getElementById('ticketTable');
-        const rows = ticketTable.getElementsByTagName('tr');
-        for (let i = 0; i < rows.length; i++) {
-            const cells = rows[i].getElementsByTagName('td');
-            if (cells.length > 0 && cells[0].textContent === ticketId) {
-                ticketTable.deleteRow(i);
-                break; // Exit the loop since we found and removed the row
-            }
-        }
-    } else {
-        alert("Failed to delete ticket.");
+  if (response.ok) {
+    alert("Ticket deleted successfully!");
+    // Find and remove the table row for the deleted ticket
+    const ticketTable = document.getElementById("ticketTable");
+    const rows = ticketTable.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+      const cells = rows[i].getElementsByTagName("td");
+      if (cells.length > 0 && cells[0].textContent === ticketId) {
+        ticketTable.deleteRow(i);
+        break; // Exit the loop since we found and removed the row
+      }
     }
+  } else {
+    alert("Failed to delete ticket.");
+  }
 }
 
 // Function to edit a ticket
- async function editTicket(ticketId) {
-      window.open("ticketEdit.html?id="+ticketId);
-
-};
+async function editTicket(ticketId) {
+  window.open("ticketEdit.html?id=" + ticketId);
+}
 
 // Event listeners
-document.getElementById('searchButton').addEventListener('click', searchTicket);
+document.getElementById("searchButton").addEventListener("click", searchTicket);
